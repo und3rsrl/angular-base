8599f02f69b865340bf596a4d1b54fbe757ce647
Alex Chiurtu <alex.chiurtu@gmail.com>
2021-06-02 20:38:07 +0300

    - Update project to Angular 12 - Fixed dataservice bug

--- angular-base/src/app/shared/services/data.service.ts
+++ angular-base/src/app/shared/services/data.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { HttpSettingsConstants } from "../constants/http-settings.constants";
 
+@Injectable({
+    providedIn: 'root'
+})
 export class DataService {
     public constructor(private httpClient: HttpClient) { }
 
